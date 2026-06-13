2193652333df0043e63b8cf61f0fde0152172349
devsdocs <id45go@gmail.com>
2026-06-13 14:26:11 +0900

    feat: add screenshot.dart example script to demonstrate basic browser automation

---
+++ bin/screenshot.dart
@@ -0,0 +1,13 @@
+import 'package:playwright_dart/playwright_dart.dart';
+
+void main() async {
+  final playwright = await Playwright.create();
+  final browser = await playwright.chromium.launch();
+  final page = await browser.newPage();
+
+  await page.goto('https://playwright.dev');
+  await page.getByRole('link', name: 'Get started').click();
+  await page.screenshot(path: 'screenshot.png');
+
+  await browser.close();
+}
