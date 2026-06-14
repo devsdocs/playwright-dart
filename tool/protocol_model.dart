fd1c0eb80d9a034ec1a652d9f51581ea2c81a6f3
devsdocs <id45go@gmail.com>
2026-06-15 01:53:22 +0900

    refactor: remove ProtocolModel class as it is no longer needed

--- tool/protocol_model.dart
+++
@@ -1,8 +0,0 @@
-class ProtocolModel {
-  ProtocolModel({this.name = ''});
-  String name;
-  Map<String, ProtocolModel> children = {};
-  factory ProtocolModel.fromJson(Map<String, dynamic> json) {
-    return ProtocolModel()..name = json['name'] as String;
-  }
-}
